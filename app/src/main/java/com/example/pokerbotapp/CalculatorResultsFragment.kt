3118f33c52259a0842c5f498c114c5bff6f1fc45
amesb23 <drayble01@gmail.com>
2023-05-11 12:49:43 -0400

    fixed spacing issues and made it navigate

--- app/src/main/java/com/example/pokerbotapp/CalculatorResultsFragment.kt
+++ app/src/main/java/com/example/pokerbotapp/CalculatorResultsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.pokerbotapp.databinding.FragmentCalculatorResultsBinding
 
 
@@ -18,6 +19,11 @@ class CalculatorResultsFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentCalculatorResultsBinding.inflate(inflater, container, false)
         val rootView = binding.root
+
+        binding.continueButton.setOnClickListener {
+            val action = CalculatorResultsFragmentDirections.actionCalculatorResultsFragmentToCalculatorFlopEntryFragment()
+            rootView.findNavController().navigate(action)
+        }
 
         return rootView
     }
